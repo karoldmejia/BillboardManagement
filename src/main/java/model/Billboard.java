9a6e2378ffbde871e7f3f7a9e1e41ee0ae7cdf81
karoldmejia <karoldmejiaoz@gmail.com>
2024-02-03 14:23:11 -0500

    initial directories

---
+++ src/main/java/model/Billboard.java
@@ -0,0 +1,32 @@
+package model;
+import java.io.*;
+
+public class Billboard implements Serializable{
+    private double width;
+    private double height;
+    private boolean inUse;
+    private String brand;
+
+    Billboard(double width, double height, boolean inUse, String brand){
+        this.width=width;
+        this.height=height;
+        this.inUse=inUse;
+        this.brand=brand;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public boolean getInUse() {
+        return inUse;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public double calculateArea(){
+        return (width*height);
+    }
+}
---
+++ src/main/java/model/InfrastructureDepartment.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InfrastructureDepartment {
+    public String BILLBOARD_FILE_NAME="data\\billboard.bbd";
+
+    InfrastructureDepartment(){
+        File billboards=new File("data\\BillboardDataExported.csv");
+
+    }
+
+    public void addBillboard(double weight, double height, boolean inUse, String brand){
+
+    }
+
+    private void saveBillboards(){
+
+    }
+    private void loadBillboards(){
+
+    }
+    public void exportDangerousBillboardReport(){
+
+    }
+    public void importData(String fn){
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+
+}
