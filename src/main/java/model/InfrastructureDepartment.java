a295d608b6442d90e4322523943c1deb68e61894
karoldmejia <karoldmejiaoz@gmail.com>
2024-02-04 12:42:39 -0500

    requirements 1 & 2

--- src/main/java/model/InfrastructureDepartment.java
+++ src/main/java/model/InfrastructureDepartment.java
@@ -1,32 +1,82 @@
 package model;
+import com.opencsv.*;
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class InfrastructureDepartment {
-    public String BILLBOARD_FILE_NAME="data\\billboard.bbd";
+    public String BILLBOARD_FILE_NAME="C:\\Users\\KAROLD\\Documents\\uni\\apo2\\BillboardManagement\\src\\main\\java\\model\\data\\BillboardDataExported.csv";
+    private String BILLBOARD_BINARY_FILE_NAME = "C:\\Users\\KAROLD\\Documents\\uni\\apo2\\BillboardManagement\\src\\main\\java\\model\\data\\BillboardDataExported.bin";
+    ArrayList<Billboard> billboards;
 
-    InfrastructureDepartment(){
-        File billboards=new File("data\\BillboardDataExported.csv");
 
+    //load file info on an arraylist
+    public InfrastructureDepartment(){
+        billboards = new ArrayList<Billboard>();
+        importData();
     }
 
-    public void addBillboard(double weight, double height, boolean inUse, String brand){
 
+    //add a new billboard to list
+    public void addBillboard(String billboardDetails){
+        String[] variables=billboardDetails.split("\\++");
+        double width = Double.parseDouble(variables[0]);
+        double height = Double.parseDouble(variables[1]);
+        boolean inUse = Boolean.parseBoolean(variables[2]);
+        String brand = variables[3];
+        Billboard newBillboard = new Billboard(width, height, inUse, brand);
+        billboards.add(newBillboard);
+        saveBillboards();
     }
 
+    //write new billboards on file
     private void saveBillboards(){
-
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(BILLBOARD_FILE_NAME))) {
+            for (Billboard billboard : billboards) {
+                String[] data = {String.valueOf(billboard.getWidth()), String.valueOf(billboard.getHeight()),
+                        String.valueOf(billboard.getInUse()), billboard.getBrand()};
+                csvWriter.writeNext(data);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(BILLBOARD_BINARY_FILE_NAME))) {
+            oos.writeObject(billboards);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    private void loadBillboards(){
 
+    //load billboards as a list (when program is starting)
+    public void loadBillboards() {
+        System.out.println("W\t\tH\t\tinUse\tBrand");
+        for (Billboard billboard : billboards) {
+            System.out.printf("%.2f\t%.2f\t%s\t%s%n", billboard.getWidth(), billboard.getHeight(),
+                    billboard.getInUse(), billboard.getBrand());
+        }
+        System.out.println("\nTOTAL: " + billboards.size() + " vallas");
     }
+
+        //creates or overwrite on a report.txt file. also shows it to the user
     public void exportDangerousBillboardReport(){
 
     }
-    public void importData(String fn){
+
+    //dunno
+    public void importData(){
+        try (CSVReader csvReader = new CSVReader(new FileReader(BILLBOARD_FILE_NAME))) {
+            String[] variables;
+            while ((variables = csvReader.readNext()) != null) {
+                    double width = Double.parseDouble(variables[0]);
+                    double height = Double.parseDouble(variables[1]);
+                    boolean inUse = Boolean.parseBoolean(variables[2]);
+                    String brand = variables[3];
+                    Billboard billboard = new Billboard(width, height, inUse, brand);
+                    billboards.add(billboard);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,19 +1,46 @@
 package ui;
+import java.util.*;
+import model.InfrastructureDepartment;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static Scanner txt;
+    static InfrastructureDepartment inDep;
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        txt = new Scanner(System.in);
+        inDep = new InfrastructureDepartment();
+        menu();
+    }
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    static void menu() {
+        int optMenu = -1;
+        System.out.printf("Welcome to Billboard Management");
+        while (optMenu != 0) {
+            System.out.print("\nPlease select an option:\n1. Add billboard\n2. Show billboards\n3. Export danger report\n" +
+                    "4. Exit\n");
+            optMenu = Main.txt.nextInt();
+            Main.txt.nextLine();
+            switch (optMenu) {
+                case 1:
+                    System.out.println("Please, enter the details for new billboard separated by (++)");
+                    String billboardDetails=Main.txt.nextLine();
+                    inDep.addBillboard(billboardDetails);
+                    break;
+                case 2:
+                    inDep.loadBillboards();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.print("Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.print("Please enter a valid option");
+                    break;
+            }
         }
     }
+
 }
